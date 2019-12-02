95d8988523db328d84521efe4815844f062d4b39
philippe-nougailon <philippe.nougaillon@gmail.com>
2019-12-02 10:53:14 +0100

    bootstrap setup

--- app/models/facture.rb
+++ app/models/facture.rb
@@ -1,2 +1,8 @@
 class Facture < ApplicationRecord
+    audited
+
+    enum etat: [:ajoutée, :envoyée, :validée, :rejetée]
+    enum anomalie: [:po, :contrat, :montant, :réception, :inconnu]
+
+    validates :etat, :anomalie, :société, :num_chrono, :cible, presence: true
 end
