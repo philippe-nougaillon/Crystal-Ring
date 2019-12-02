0920013214e008f98c0c80d65ce086f016c60524
philippe-nougailon <philippe.nougaillon@gmail.com>
2019-12-02 11:16:35 +0100

    Facture scan file

--- app/controllers/factures_controller.rb
+++ app/controllers/factures_controller.rb
@@ -69,6 +69,6 @@ class FacturesController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def facture_params
-      params.require(:facture).permit(:etat, :anomalie, :num_chrono, :par, :société, :cible)
+      params.require(:facture).permit(:etat, :anomalie, :num_chrono, :par, :société, :cible, :scan)
     end
 end
--- app/models/facture.rb
+++ app/models/facture.rb
@@ -5,4 +5,15 @@ class Facture < ApplicationRecord
     enum anomalie: [:po, :contrat, :montant, :réception, :inconnu]
 
     validates :etat, :anomalie, :société, :num_chrono, :cible, presence: true
+
+    has_one_attached :scan
+
+    after_initialize do
+        if self.new_record?
+          # values will be available for new record forms.
+          self.etat = 0
+          self.anomalie = 0
+        end
+    end
+
 end
