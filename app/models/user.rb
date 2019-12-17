0cbe3b838577afb230a09e4bcdea1d2d1cd8aed4
philippe-nougailon <philippe.nougaillon@gmail.com>
2019-12-17 14:19:14 +0100

    User trackable

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,14 +1,14 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, #:registerable,
-         :recoverable, :rememberable, :validatable
-
-  audited
-
   extend FriendlyId
   friendly_id :slug_candidates, use: :slugged
 
+  audited
+
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, #:registerable,
+         :recoverable, :rememberable, :validatable,
+         :trackable
 
 private
     # only one candidate for an nice id; one random UDID
