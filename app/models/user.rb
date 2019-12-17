c1d4283854008edf7dbb37bc86838ada71bd8d33
philippe-nougailon <philippe.nougaillon@gmail.com>
2019-12-17 13:56:21 +0100

    User slug

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,17 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, #:registerable,
          :recoverable, :rememberable, :validatable
+
+  audited
+
+  extend FriendlyId
+  friendly_id :slug_candidates, use: :slugged
+
+
+private
+    # only one candidate for an nice id; one random UDID
+    def slug_candidates
+      [SecureRandom.uuid]
+    end
+
 end
