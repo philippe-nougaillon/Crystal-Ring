972b64c17074f6e14841516974a94966dea591b6
philippe-nougailon <philippe.nougaillon@gmail.com>
2020-01-10 20:30:32 +0100

    améliorations diverses

--- app/models/cible.rb
+++ app/models/cible.rb
@@ -3,4 +3,7 @@ class Cible < ApplicationRecord
   audited associated_with: :facture
 
   default_scope { order(Arel.sql("cibles.id")) } 
+
+  scope :sans_réponse, -> { where(repondu_le: nil) }
+
 end
