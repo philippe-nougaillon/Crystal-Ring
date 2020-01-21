af3025747c755f70c7a9a6f33cc2d91f64885f9c
philippe-nougailon <philippe.nougaillon@gmail.com>
2020-01-21 13:49:56 +0100

    v0.9.g

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,7 @@ private
     def set_layout_variables
       @ctrl = params[:controller]
       @title = "AnoFacto "
-      @version = "v0.9.f"
+      @version = "v0.9.g"
     end
 
     def user_not_authorized
