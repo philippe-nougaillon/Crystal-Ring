36e48454d9454cfa0d2d0ef5abd239de360ba0c8
philippe-nougailon <philippe.nougaillon@gmail.com>
2020-01-26 17:12:52 +0100

    corrections mineures dans code html page factures

--- app/controllers/factures_controller.rb
+++ app/controllers/factures_controller.rb
@@ -177,7 +177,7 @@ class FacturesController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def facture_params
-      params.require(:facture).permit(:anomalie, :num_chrono, :par, :société, :scan, :montantHT, :commentaires,
+      params.require(:facture).permit(:anomalie, :num_chrono, :par, :société, :scan, :montantHT, :commentaires, :workflow_state,
                                     cibles_attributes: [:id, :email, :répondu_le, :réponse, :_destroy])
     end
 end
