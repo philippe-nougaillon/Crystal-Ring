e35ccecb7bf1980ac74e3c7c77aa183712258007
philippe-nougailon <philippe.nougaillon@gmail.com>
2020-04-14 12:03:35 +0200

    mail subject

--- app/mailers/facture_mailer.rb
+++ app/mailers/facture_mailer.rb
@@ -2,7 +2,7 @@ class FactureMailer < ApplicationMailer
     def notification_email
         @cible= params[:cible]
         @facture = @cible.facture
-        subject = "Facture n° #{@facture.num_chrono} du fournisseur #{@facture.société} en anomalie" 
+        subject = "Facture n° #{@facture.num_chrono} du fournisseur '#{@facture.société}' en anomalie" 
     
         mail(to: @cible.email, subject: subject)
     end
