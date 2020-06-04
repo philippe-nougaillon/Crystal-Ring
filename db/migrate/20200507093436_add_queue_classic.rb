4921363ae71766add89790cfd098a4f3aeeaeff8
philippe-nougailon <philippe.nougaillon@gmail.com>
2020-06-04 13:00:19 +0200

    Désactivation QueueClassic migrations

--- db/migrate/20200507093436_add_queue_classic.rb
+++ db/migrate/20200507093436_add_queue_classic.rb
@@ -1,11 +1,11 @@
 # frozen_string_literal: true
 
-class AddQueueClassic < ActiveRecord::Migration[4.2]
-  def self.up
-    QC::Setup.create
-  end
+# class AddQueueClassic < ActiveRecord::Migration[4.2]
+#   def self.up
+#     QC::Setup.create
+#   end
 
-  def self.down
-    QC::Setup.drop
-  end
-end
+#   def self.down
+#     QC::Setup.drop
+#   end
+# end
