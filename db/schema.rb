314c682940ab0a320c6f61792399269f4d8dfbde
philippe-nougailon <philippe.nougaillon@gmail.com>
2020-06-24 15:12:13 +0200

    schema updated

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_24_122125) do
+ActiveRecord::Schema.define(version: 2020_06_24_130507) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -124,7 +124,7 @@ ActiveRecord::Schema.define(version: 2020_06_24_122125) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.boolean "admin"
+    t.boolean "admin", default: true
     t.string "slug"
     t.integer "sign_in_count", default: 0, null: false
     t.datetime "current_sign_in_at"
