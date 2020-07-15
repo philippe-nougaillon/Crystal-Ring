23d2ebe5ce456ccc450f9e70e4400d7ad1f0d5b7
philippe-nougailon <philippe.nougaillon@gmail.com>
2020-07-15 10:36:16 +0200

    chgt SMTP settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -119,12 +119,12 @@ Rails.application.configure do
     :address        => ENV['MAILGUN_SMTP_SERVER'],
     :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
     :password       => ENV['MAILGUN_SMTP_PASSWORD'],
-    :domain         => 'anofacto.heroku.com',
+    :domain         => 'grohe.heroku.com',
     :authentication => :plain,
   }
   ActionMailer::Base.delivery_method = :smtp
 
-  config.action_mailer.default_url_options = { host: 'anofacto.herokuapp.com', protocol: 'https' }
+  config.action_mailer.default_url_options = { host: 'grohe.herokuapp.com', protocol: 'https' }
 
   # Exception Notification
   # Rails.application.config.middleware.use ExceptionNotification::Rack,
