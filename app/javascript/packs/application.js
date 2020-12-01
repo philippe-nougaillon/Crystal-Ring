3bf0474cae308712104caaed3a427dc1b345e791
philnoug <philnoug@penguin>
2020-12-01 16:22:49 +0100

    Stimulus activé

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -16,10 +16,8 @@ require("channels")
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
 
-import 'bootstrap'
-
-document.addEventListener("turbolinks:load", () => {
-  $('[data-toggle="tooltip"]').tooltip()
-})
+// document.addEventListener("turbolinks:load", () => {
+//   $('[data-toggle="tooltip"]').tooltip()
+// })
 
 import "controllers"
